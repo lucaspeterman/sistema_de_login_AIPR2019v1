aac4ce11b179b7808d4c94dc57f7c3d2e853eaf6
Lucas Petermann <lucaspetermann78@gmail.com>
2019-08-23 15:08:44 -0300

    Conaxão com banco de dados

---
+++ configDB.php
@@ -0,0 +1,16 @@
+<?php 
+//configDb.php
+
+//Dados para escolha do DataBase(DB)
+$dbhost = "localhost";
+$dbuser = "root"; //Usuário raiz (Rute)
+$dbpass = "";
+$dbname = "sistemaDeLogin";
+
+//Conexão com o banco de dados
+$conecta = new mysqli($dbhost,$dbuser, $dbpass, $dbname);
+if ($conecta-> connect_error){
+    die("Não foi possível conectar ao banco de dados: " . $conecta->connect_error);
+}else{
+    echo "<h1> conectou no BD Manowwwwww!</h1>";
+}

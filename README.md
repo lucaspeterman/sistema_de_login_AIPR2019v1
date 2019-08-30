fbbfd5c3a1de8fe63b04dccac5093cd7d16ae21f
lucaspetermann <lucaspetermann78@gmail.com>
2019-08-30 16:28:43 -0300

    cadastro

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # sistema_de_login_AIPR2019v1
 Sistema de Login com registro de novo usuário e recuperação de senha
 
+## aula CÊXXXX - 30/08/2019
+Ùltima sexte-feira de agosto de 2019
+Cadastro d usuário
+
 ##aula05 - 23/08/2019
 Criação de banco de dados e cadastro de usuário
 
--- recebe.php
+++ recebe.php
@@ -1,40 +1,58 @@
 <?php
-
+//É necessário fazer a conexão com o Banco de Dados
 require_once "configDB.php";
-
-function verificar_entrada($entrada){
-    $saida = trim($entrada);
-    $saida = stripslashes($saida);
+function verificar_entrada($entrada)
+{
+    $saida = trim($entrada); //Remove espaços antes e depois
+    $saida = stripslashes($saida); //remove as barras
     $saida = htmlspecialchars($saida);
     return $saida;
 }
-
-if(isset($_POST['action']) && $_POST['action']== 'cadastro'){
-    $nomeCompleto = verificar_entrada($_POST ['nomeCompleto']);
-    $nomeUsuario = verificar_entrada($_POSt ['nomeUsuário']);
-    $emailUsuario = verificar_entrada($_POST ['emailUsuário']);
-    $senhaUsuario = verificar_entrada($_POST ['senhaUsuário']);
-    $senhaConfirma = verificar_entrada($_POSt ['senhaUsuário']);
+if (
+    isset($_POST['action']) &&
+    $_POST['action'] == 'cadastro'
+) {
+    //Pegar os campos do formulário
+    $nomeCompleto = verificar_entrada($_POST['nomeCompleto']);
+    $nomeUsuario = verificar_entrada($_POST['nomeUsuário']);
+    $emailUsuario = verificar_entrada($_POST['emailUsuário']);
+    $senhaUsuario = verificar_entrada($_POST['senhaUsuário']);
+    $senhaConfirma = verificar_entrada($_POST['senhaConfirma']);
     $concordar = $_POST['concordar'];
     $dataCriacao = date("Y-m-d H:i:s");
 
-    echo "<h5>Nome completo: $nomeCompleto</h5>";
-    echo "<h5>Nome Usuário: $nomeUsuario</h5>";
-    echo "<h5>E-mail Usuário: $emailUsuario</h5>";
-    echo "<h5>Senha Usuário: $senhaUsuario</h5>";
-    echo "<h5>Senha Confirma: $senhaComfirma</h5>";
-    echo "<h5>Concordar: $concordar</h5>";
-    echo "<h5>Data Crição: $dataCriacao</h5>";
 
-    if($senha!= $senhaC){
-        echo "<h1>As senhas não conferem</h1>";
-        exit();
-    }else{
-        echo "<h5>senha codificada: $senha</h5>";
-    }
 
+    //Hash de senha / Codificação de senha em 40 caracteres
     $senha = sha1($senhaUsuario);
     $senhaC = sha1($senhaConfirma);
-    }else{
-    echo"<h1 style= 'color:red'> Esta página não pode ser acessada diretamente</h1>";
+
+    if ($senha != $senhaC) {
+        echo "<h1>As senhas não conferem</h1>";
+        exit();
+    } else {
+        //echo "<h5> senha codificada: $senha</h5>";
+        //Verificar se o usuário ja existe no banco de dados
+        $sql = $conecta->prepare("SELECT nomeUsuario, email FROM usuario WHERE nomeUsuario = ? OR email = ?");
+        $sql->bind_param("SS",$nomeUsuario, $emailUsuario);
+        $sql->execute();
+        $resultado = $sql->get_result();
+        $linha = $rsultado->fetch_arrey(MYSQLI_ASSOC);
+        if($linha['nomeUsuario'] == $nomeUsuario){
+            echo "<p>Nome de usuário indisponival, tente outro</p>";
+        }elseif ($linha['email'] == $emailUsuario){
+            echo "<p>E-mail ja em usso tente outro</p>";
+        }else{
+            $sql = $conecta->prepare("INSERT into usuario (nome, nomeUsuario, email, senha, dataCriacao)values(?,?,?,?,?)");
+            $sql->bind_param("sssss",$nomeCompleto, $nomeUsuario, $emailUsuario, $senha, $dataCriacao);
+            if($sql->execute()){
+                echo "<p>Registrado com socesso</p>";
+            }else{
+                echo "<p>Algo deu errado. tente novamente</p>";
+            }
+        }
     }
+} else {
+    echo "<h1 style='color:red'>Esta página não pode 
+    ser acessada diretamente</h1>";
+}

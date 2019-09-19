89169ca97e6c1e4b2dbc89037d6571beca387918
Lucas Petermann <lucaspetermann78@gmail.com>
2019-09-19 15:07:44 -0300

    passo1

--- recebe.php
+++ recebe.php
@@ -1,72 +1,80 @@
 <?php
+
 //Inicializando a sessão
 session_start();
 
 //É necessário fazer a conexão com o Banco de Dados
 require_once "configDB.php";
-
 function verificar_entrada($entrada)
 {
-    $saida = trim($entrada); //Remove espaços antes e depois
-    $saida = stripslashes($saida); //remove as barras
+    $saida = trim($entrada); //Remove espaços antes e depois.
+    $saida = stripslashes($saida); //Remove as barras(\/).
     $saida = htmlspecialchars($saida);
     return $saida;
 }
 
-if (
-    isset($_POST['action']) &&
-    $_POST['action'] == 'login'
-) {
+if (isset($_POST['action']) && $_POST['action'] == 'senha') {
+    //echo "<strong>Recuperação de Senha</strong>";
+    $emailSenha = verificar_entrada($_POST['emailSenha']);
+    $sql = $conecta->prepare("SELECT idUsuario FROM usuario WHERE email = ?");
+    $sql->bind_param("s", $emailSenha);
+    $sql->execute();
+    $resultado = $sql->get_result();
+    if ($resultado->num_rows > 0) {
+        //Existe o usuário no Banco de Dados
+        //Só para testar / debug
+        //echo '<p class="text-success">E-mail encontrado</p>';
+        $frase = "BatAtinh4Quando!@#$%NAscexexeEparr4maPeloChao&*";
+        $frase_secreta = str_shuffle($frase);
+        $token = substr($frase_secreta, 0, 10);
+        echo "<p>$token</p>";
+    } else {
+        echo '<p class="text-danger">E-mail não encontrado</p>';
+    }
+} else if (isset($_POST['action']) && $_POST['action'] == 'login') {
     //Verificação e Login do usuário
     $nomeUsuario = verificar_entrada($_POST['nomeUsuario']);
     $senhaUsuario = verificar_entrada($_POST['senhaUsuario']);
     $senha = sha1($senhaUsuario);
+
     //Para teste
     //echo "<br>Usuário: $nomeUsuario <br> senha: $senha";
-    $sql = $conecta->prepare("SELECT * FROM usuario WHERE 
-        nomeUsuario = ? AND senha = ?");
+    $sql = $conecta->prepare("SELECT * FROM usuario WHERE nomeUsuario = ? AND senha = ?");
     $sql->bind_param("ss", $nomeUsuario, $senha);
     $sql->execute();
-
     $busca = $sql->fetch();
 
-    if ($busca != null) {
-        //Colocando o nome do usuário na Sessão
+    if ($busca != null) { //! = è diferente
+        //Colocando o nome do usuário na sessão
         $_SESSION['nomeUsuario'] = $nomeUsuario;
         echo "ok";
-
-        if(!empty($_POST['lembrar'])){
-            //Se não estiver vazio 
-            //armazenar Login e senha no cookie
-            setcookie("nomeUsuario", $nomeUsuario, time()+(30*24*60*60));
-            setcookie("senahUsuario", $senhaUsuario, time()+(30*24*60*60));//30 dias em segundos
-        }else{
-            //se estiver vazio
-            setcookie("nomeUsuario","");
-            setcookie("senhaUsuario","");
+        if (!empty($_POST['lembrar'])) {
+            //Se não estiver vazio
+            //Armazenar Login e Senha no Cookie
+            setcookie("nomeUsuario", $nomeUsuario, time() + (30 * 24 * 60 * 60)); //30 dias em segundos
+            setcookie("senhaUsuario", $senhaUsuario, time() + (30 * 24 * 60 * 60)); //30 dias em segundos
+        } else {
+            //Se estive vazio
+            setcookie("nomeUsuario", "");
+            setcookie("senhaUsuario", "");
         }
-
     } else {
-        echo "usuário e senha não conferem!";
+        echo "Usuário e senha não conferem!";
     }
-
-} else if (
-    isset($_POST['action']) &&
-    $_POST['action'] == 'cadastro'
-) {
+} else if (isset($_POST['action']) && $_POST['action'] == 'cadastrar') {
     //Cadastro de um novo usuário
+    //Pegar os campos do formulário
     //Pegar os campos do formulário
     $nomeCompleto = verificar_entrada($_POST['nomeCompleto']);
     $nomeUsuario = verificar_entrada($_POST['nomeUsuário']);
     $emailUsuario = verificar_entrada($_POST['emailUsuário']);
     $senhaUsuario = verificar_entrada($_POST['senhaUsuário']);
     $senhaConfirma = verificar_entrada($_POST['senhaConfirma']);
-    $urlimg = verificar_entrada($_POST['imgurl']);
+    $url = verificar_entrada($_POST['imgUrl']);
     $concordar = $_POST['concordar'];
     $dataCriacao = date("Y-m-d H:i:s");
 
-
-    //Hash de senha / Codificação de senha em 40 caracteres
+    //hash de senha / codificação de senha em 40 carácteres
     $senha = sha1($senhaUsuario);
     $senhaC = sha1($senhaConfirma);
 
@@ -74,31 +82,31 @@ if (
         echo "<h1>As senhas não conferem</h1>";
         exit();
     } else {
-        //echo "<h5> senha codificada: $senha</h5>";
-        //Verificar se o usuário já existe no banco de dados
-        $sql = $conecta->prepare("SELECT nomeUsuario, email 
-        FROM usuario WHERE nomeUsuario = ? OR email = ?");
+        //echo "<h5>senha codificada: $senha</h5>";
+        //Verificar se o usuário ja existe no banco de dados
+        $sql = $conecta->prepare("SELECT nomeUsuario, email FROM usuario WHERE nomeUsuario = ? OR email = ?");
+
         //Substitui cada ? por uma string abaixo
         $sql->bind_param("ss", $nomeUsuario, $emailUsuario);
         $sql->execute();
+
         $resultado = $sql->get_result();
         $linha = $resultado->fetch_array(MYSQLI_ASSOC);
+
         if ($linha['nomeUsuario'] == $nomeUsuario) {
             echo "<p>Nome de usuário indisponível, tente outro</p>";
         } elseif ($linha['email'] == $emailUsuario) {
             echo "<p>E-mail já em uso, tente outro</p>";
-        } else { 
-            //Cadastro de usuário
-            $sql = $conecta->prepare("INSERT into usuario (nome, nomeUsuario, email, senha, urlimg, dataCriacao) values(?, ?, ?, ?, ?, ? )");
-            $sql->bind_param("ssssss", $nomeCompleto, $nomeUsuario, $emailUsuario, $senha, $urlimg, $dataCriacao);
+        } else {
+            $sql = $conecta->prepare("INSERT into usuario (nome, nomeUsuario, email, senha, urlImg,dataCriacao) values(?, ?, ?, ?, ?, ?)");
+            $sql->bind_param("ssssss", $nomeCompleto, $nomeUsuario, $emailUsuario, $senha, $url, $dataCriacao);
             if ($sql->execute()) {
                 echo "<p>Registrado com sucesso</p>";
             } else {
-                echo "<p>Algo deu errado. Tente outra vez.</p>";
+                echo "<p>Algo deu errado. Tente ou vez.</p>";
             }
         }
     }
 } else {
-    echo "<h1 style='color:red'>Esta página não pode 
-    ser acessada diretamente</h1>";
+    echo "<h1 style='color:red'>Esta página não pode ser acessada diretamente</h1>";
 }
